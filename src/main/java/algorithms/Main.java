16d7bf4fb44d298f7c9da60a3bd7fa88c890cb3c
kausartukezan <tukezhan.kausar@gmail.com>
2025-09-27 21:53:26 +0500

    feat: finalize Main.java for experiments

--- src/main/java/algorithms/Main.java
+++ src/main/java/algorithms/Main.java
@@ -4,30 +4,35 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        int[] sizes = {100, 500, 1000, 5000}; // размеры массивов
         Random random = new Random();
 
-        int[] arr1 = random.ints(20, 0, 100).toArray();
-        Metrics m1 = new Metrics();
-        MergeSort.sort(arr1.clone(), m1);
-        System.out.println("MergeSort: " + m1);
+        for (int n : sizes) {
+            System.out.println("\n=== n = " + n + " ===");
 
-        int[] arr2 = random.ints(20, 0, 100).toArray();
-        Metrics m2 = new Metrics();
-        QuickSort.sort(arr2.clone(), m2);
-        System.out.println("QuickSort: " + m2);
+            int[] arr1 = random.ints(n, 0, 10000).toArray();
+            Metrics m1 = new Metrics();
+            MergeSort.sort(arr1.clone(), m1);
+            System.out.println("MergeSort: " + m1);
 
-        int[] arr3 = random.ints(20, 0, 100).toArray();
-        Metrics m3 = new Metrics();
-        int k = arr3.length / 2;
-        int median = DeterministicSelect.select(arr3.clone(), k, m3);
-        System.out.println("DeterministicSelect (median = " + median + "): " + m3);
+            int[] arr2 = random.ints(n, 0, 10000).toArray();
+            Metrics m2 = new Metrics();
+            QuickSort.sort(arr2.clone(), m2);
+            System.out.println("QuickSort: " + m2);
 
-        ClosestPair.Point[] points = new ClosestPair.Point[10];
-        for (int i = 0; i < points.length; i++) {
-            points[i] = new ClosestPair.Point(random.nextDouble() * 100, random.nextDouble() * 100);
+            int[] arr3 = random.ints(n, 0, 10000).toArray();
+            Metrics m3 = new Metrics();
+            int k = n / 2;
+            int median = DeterministicSelect.select(arr3.clone(), k, m3);
+            System.out.println("DeterministicSelect (median = " + median + "): " + m3);
+
+            ClosestPair.Point[] points = new ClosestPair.Point[n];
+            for (int i = 0; i < n; i++) {
+                points[i] = new ClosestPair.Point(random.nextDouble() * 1000, random.nextDouble() * 1000);
+            }
+            Metrics m4 = new Metrics();
+            double minDist = ClosestPair.findClosestPair(points, m4);
+            System.out.println("ClosestPair (minDist = " + minDist + "): " + m4);
         }
-        Metrics m4 = new Metrics();
-        double minDist = ClosestPair.findClosestPair(points, m4);
-        System.out.println("ClosestPair (minDist = " + minDist + "): " + m4);
     }
 }
